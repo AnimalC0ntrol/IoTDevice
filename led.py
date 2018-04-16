634c8b59332d441cd17d70cdbcce8c92a02a5113
Asgeir Nilsen <ani082@post.uit.no>
2018-04-16 11:11:55 +0200

    changed the naming

--- led.py
+++ led.py
@@ -6,9 +6,10 @@ import pycom
 class Led():
     def __init__(self, pinnumber):
         self.light = Pin(pinnumber, mode=Pin.OUT, value=1)
+        self.light(0)
 
-    def blink(self, delay, blinkingtime):
-        numtimes = blinkingtime / delay
+    def blink(self, delay, duration):
+        numtimes = duration / delay
         for i in range(0, numtimes):
             self.light(1)
             sleep(delay)
