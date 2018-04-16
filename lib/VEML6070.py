319c9b7ca3e1bd12817db621d480e8cfcb71797d
Asgeir Nilsen <ani082@post.uit.no>
2018-04-16 11:11:04 +0200

    added the UV sensor

---
+++ lib/VEML6070.py
@@ -0,0 +1,13 @@
+# Simple driver for the VEML6070 uv light sensor
+from machine import I2C
+class VEML6070_UV:
+    def __init__(self, i2c, addr=0x38):
+        self.i2c = i2c
+        self.addr = addr
+        self.value = 0
+        self.i2c.writeto(addr, bytes([0x02])) # start continuos 1 Lux readings every 120ms
+
+    def read(self):
+        data = self.i2c.readfrom(self.addr, 2)
+        self.value = (((data[0] << 8) + data[1]) * 1200) // 1000
+        return self.value
--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import machine
 import os
 import pycom
 import _thread
+import VEML6070
 from time import sleep
 from startiot import Startiot
 from common import *
-from machine import Pin
+from machine import Pin, I2C
 from L76GNSS import L76GNSS
 from LIS2HH12 import LIS2HH12
 from pytrack import Pytrack
@@ -17,24 +18,25 @@ class AnimalAlert():
 	def __init__(self):
 		self.iot = Startiot()
 		self.iot.connect(False)
-
+		
 		py = Pytrack()
 		self.gps = L76GNSS(py, timeout = 30)
-		self.acc = LIS2HH12(py)
+		#self.acc = LIS2HH12(py)
 
 		self.centerpir = Longpir("P11")
 		self.led = Led("P10")
 		
+		"""
+		self.i2c = I2C(0, I2C.MASTER, baudrate=100000, pins = ("P4", "P8"))
+		self.uv_sensor = VEML6070.VEML6070_UV(self.i2c)
+		"""
+
 		pycom.heartbeat(False) # disable the blue blinking
 		#self.sidepir = Shortpir("P11", "P10") 	
 
 	def get_gps(self):
 		(m_lat, m_lng) = self.gps.coordinates()
 		return(m_lat, m_lng)
-
-
-	def send(self, data):
-		self.iot.send(data)
 
 	def run(self):
 		# main loop
@@ -43,14 +45,20 @@ class AnimalAlert():
 			print(val)
 
 			if val == 1:
-				_thread.start_new_thread(self.led.blink, (1, 10))
-				(m_lat, m_lng) = self.get_gps(8)
+				self.led.blink(1,10)
+				
+			
+				(m_lat, m_lng) = self.get_gps()
 				data = "{},{},{}".format(val, m_lat, m_lng)
+				"""
+				uv = self.uv_sensor.read()
+				print(uv)
+				"""
 				print(data)
-				self.send
+				self.iot.send(data)
 
 			self.led.stop
-			sleep(1)
+			sleep(0.1)
 
 if __name__=="__main__":
 	aa = AnimalAlert()
