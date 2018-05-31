e92046f7fcfbce6c9aa18c1ed7309455f530323b
Asgeir Nilsen <ani082@post.uit.no>
2018-05-31 12:37:23 +0200

    fiks færdig

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from pir import *
 from led import Led
 from common import *
 from pysense import Pysense
-from SI7006A20 import SI7006A20
 
 
 #from L76GNSS import L76GNSS
@@ -22,20 +21,18 @@ class AnimalAlert():
 		self.iot = Startiot()
 		self.iot.connect(False)
 		self.py = Pysense()
-		self.si = SI7006A20(self.py)
-
 		"""
 		self.gps = L76GNSS(py, timeout = 30)
 		(self.lat, self.lng) = self.gps.coordinates()
 		self.counter = 0
 		"""
+		self.state = STATE_IDLE
 
 		self.rightpir = Pir("P11")
 		self.centerpir = Pir("P2") 
 		self.leftpir = Pir("P9")
 		self.led = Led("P10")
 		self.i2c = I2C(1, I2C.MASTER, baudrate=100000, pins = ("P4", "P8"))
-		
 		self.uv_sensor = VEML6070.VEML6070_UV(self.i2c)
 		self.baseline = []
 		pycom.heartbeat(False) # disable the blue blinking
@@ -64,13 +61,21 @@ class AnimalAlert():
 		return average
 
 	def notify(self, left, center, right):
-		self.led.blink(LED_DELAY, LED_DURATION)		
-		
-		#(lat, lng) = self.get_gps()
-
-		#data = "{},{},{},{},{},{}".format(left, center, right, lat, lng, uv)
 		data = "{},{},{}".format(left, center, right)
-		self.iot.send(data)
+		if center == 1: 
+			self.state = STATE_WARNING
+			self.iot.send(data)
+			print("sent to startIoT")
+			self.led.blink(LED_DELAY, LED_DURATION)
+
+		elif left == 1 or right == 1:
+			self.state = STATE_ALERT
+			self.iot.send(data)
+			print("sent to startIoT")
+			self.led.blink(LED_DELAY, LED_DURATION)
+
+		#(lat, lng) = self.get_gps()
+		#data = "{},{},{},{},{},{}".format(left, center, right, lat, lng, uv)
 
 	def run(self):
 		# main loop
@@ -79,17 +84,16 @@ class AnimalAlert():
 			rightval = self.rightpir.check_sensor()
 			lefval = self.leftpir.check_sensor()
 			uv = self.uv_sensor.read()
-			battery = self.py.read_battery_voltage()
-			temp = self.si.temperature()
-			
+	
 			average = self.uv_baseline(uv)
 			print(lefval, centerval, rightval, uv, average)
 
 			if centerval == 1 or lefval == 1 or rightval == 1:
 				if uv > average + BASELINE_DIFF:
 					pass
-				#else: 
-				self.notify(lefval, centerval, rightval)
+				else: 
+					self.notify(lefval, centerval, rightval)
+					self.state = STATE_IDLE
 
 			self.led.stop
 			sleep(SLEEPTIME_MAIN)
